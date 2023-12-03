b880d75e0df587f1366ba13f57c9735bbe7709c3
황진홍 <william1412.surya@gmail.com>
2023-12-03 11:30:14 +0700

    Dashboard & Gym Session Update

--- client/app/src/pages/UserGymSession.js
+++ client/app/src/pages/UserGymSession.js
@@ -1,6 +1,67 @@
+import styles from "../assets/UserStyle.module.css";
+
 function UserGymSession() {
   return(
-    <div className=''>UserGymSession</div>
+    <div className={`${styles.userDashboard}`}>
+      <div className={`${styles.gymSessionHeader}`}>
+        <div className={`${styles.gymSessionSubTitle}`}>
+          <div className={`${styles.textTitle}`}>AVAILABLE SESSION</div>
+          <div className={`${styles.textContent}`}>Here are the gym sessions available now.</div>
+        </div>
+        <img src="https://emilypost.com/client_media/images/blogs/everyday-gym.jpg" alt="gymimage"></img>
+      </div>
+      <div className={`${styles.headerRibbon}`}>
+        <div className={`${styles.ribbonText}`}>
+        AVAILABLE SESSION * AVAILABLE SESSION * AVAILABLE SESSION * AVAILABLE SESSION
+        </div>
+      </div>
+      <div className={`${styles.section}`}>
+        <div className={`${styles.gymSessionGalerry}`}>
+          <div className={`${styles.item}`}>
+            <div className={`${styles.gymSessionGalerryContent}`}>
+              <div className={`${styles.textTitle}`}>FIT CYCLE</div>
+              <div className={`${styles.sessionDetail}`}>
+                <div className={`${styles.time}`}><svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg> 09:00</div>
+                <div className={`${styles.capacity}`}>13 Slot</div>
+              </div>
+            </div>
+            <div className={`${styles.galleryBanner}`}>
+              <div className={`${styles.rotateText}`}>BOOK SEKARANG</div>
+            </div>
+            <img src="https://emilypost.com/client_media/images/blogs/everyday-gym.jpg" alt="gymimage"></img>
+          </div>
+          <div className={`${styles.item}`}>
+            <div className={`${styles.gymSessionGalerryContent}`}>
+              <div className={`${styles.textTitle}`}>BODY COMBAT</div>
+              <div className={`${styles.sessionDetail}`}>
+                <div className={`${styles.time}`}><svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg> 09:00</div>
+                <div className={`${styles.capacity}`}>24 Slot</div>
+              </div>
+            </div>
+            <div className={`${styles.galleryBanner}`}>
+              <div className={`${styles.rotateText}`}>BOOK SEKARANG</div>
+            </div>
+            <img src="https://emilypost.com/client_media/images/blogs/everyday-gym.jpg" alt="gymimage"></img>
+          </div>
+          <div className={`${styles.item}`}>
+            <div className={`${styles.gymSessionGalerryContent}`}>
+              <div className={`${styles.textTitle}`}>ZUMBA</div>
+              <div className={`${styles.sessionDetail}`}>
+                <div className={`${styles.time}`}><svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg> 10:00</div>
+                <div className={`${styles.capacity}`}>25 Slot</div>
+              </div>
+            </div>
+            <div className={`${styles.galleryBanner}`}>
+              <div className={`${styles.rotateText}`}>BOOK SEKARANG</div>
+            </div>
+            <img src="https://emilypost.com/client_media/images/blogs/everyday-gym.jpg" alt="gymimage"></img>
+          </div>
+        </div>
+      </div>
+      <div className={`${styles.footer}`}>
+        <div>&copy; 2023 Tweak Gym. All rights reserved.</div>
+      </div>
+    </div>
   );
 }
 
--- client/app/src/pages/UserNavBar.js
+++ client/app/src/pages/UserNavBar.js
@@ -1,21 +1,55 @@
+import { useNavigate, useLocation } from 'react-router-dom';
 import styles from "../assets/UserStyle.module.css";
+
 function UserNavBar() {
-  return(
+  const navigate = useNavigate();
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  const handleHomeClick = () => {
+    navigate('/');
+  };
+
+  const handleBookingClick = () => {
+    navigate('/gym-session');
+  };
+
+  const handleQrButtonClick = () => {
+    navigate('/');
+  };
+
+  const handleHistoryClick = () => {
+    navigate('/history');
+  };
+
+  const handlePoinClick = () => {
+    navigate('/user-poin');
+  };
+
+  return (
     <div className={`${styles.navbarContainer}`}>
       <div className={`${styles.navbar} ${styles.textWhite}`}>
-        <div className={`${styles.navButton} ${styles.active}`}>Home</div>
-        <div className={`${styles.navButton}`}>Booking</div>
-        <div className={`${styles.qrButton} ${styles.bgLight}`}><svg xmlns="http://www.w3.org/2000/svg" height="35" width="35" viewBox="0 0 448 512"><path d="M349.4 44.6c5.9-13.7 1.5-29.7-10.6-38.5s-28.6-8-39.9 1.8l-256 224c-10 8.8-13.6 22.9-8.9 35.3S50.7 288 64 288H175.5L98.6 467.4c-5.9 13.7-1.5 29.7 10.6 38.5s28.6 8 39.9-1.8l256-224c10-8.8 13.6-22.9 8.9-35.3s-16.6-20.7-30-20.7H272.5L349.4 44.6z"/></svg></div>
-        <div className={`${styles.navButton}`}>History</div>
-        <div className={`${styles.navButton}`}>Poin</div>
+        <div className={`${styles.navButton} ${currentPath === '/' && styles.active}`} onClick={handleHomeClick}>
+          Home
+        </div>
+        <div className={`${styles.navButton} ${currentPath === '/gym-session' && styles.active}`} onClick={handleBookingClick}>
+          Booking
+        </div>
+        <div className={`${styles.qrButton} ${styles.bgLight}`} onClick={handleQrButtonClick}>
+          <svg xmlns="http://www.w3.org/2000/svg" height="35" width="35" viewBox="0 0 448 512">
+            <path d="M349.4 44.6c5.9-13.7 1.5-29.7-10.6-38.5s-28.6-8-39.9 1.8l-256 224c-10 8.8-13.6 22.9-8.9 35.3S50.7 288 64 288H175.5L98.6 467.4c-5.9 13.7-1.5 29.7 10.6 38.5s28.6 8 39.9-1.8l256-224c10-8.8 13.6-22.9 8.9-35.3s-16.6-20.7-30-20.7H272.5L349.4 44.6z"/>
+          </svg>
+        </div>
+        <div className={`${styles.navButton} ${currentPath === '/history' && styles.active}`} onClick={handleHistoryClick}>
+          History
+        </div>
+        <div className={`${styles.navButton} ${currentPath === '/user-poin' && styles.active}`} onClick={handlePoinClick}>
+          Poin
+        </div>
       </div>
-      <div className={`${styles.navbarBG}`}>
-        
-      </div>
+      <div className={`${styles.navbarBG}`}></div>
     </div>
   );
 }
 
-export {
-  UserNavBar,
-};
+export { UserNavBar };
