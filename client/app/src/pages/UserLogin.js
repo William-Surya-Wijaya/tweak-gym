0c8d015f6082f64e59cba52ca40c37b31b3492ab
황진홍 <william1412.surya@gmail.com>
2023-12-03 19:58:30 +0700

    Add Login & Register Design

--- client/app/src/pages/UserLogin.js
+++ client/app/src/pages/UserLogin.js
@@ -1,6 +1,33 @@
+import React from 'react';
+import styles from "../assets/UserStyle.module.css";
+
 function UserLogin() {
-  return(
-    <div className=''>UserLogin</div>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+      //
+  };
+
+  return (
+    <div className={`${styles.loginPage}`}>
+      <div className={`${styles.gymLogo}`}>
+        <img className={`${styles.topLogo}`} src="https://cdn.dribbble.com/users/6177297/screenshots/20141188/media/766dc94001d4ef551ba405786fbbb323.jpg?resize=400x0" alt="gym-logo"></img>
+      </div>
+      <div className={`${styles.textTitle}`}>TWEAK GYM</div>
+      <div className={`${styles.subTitle}`}>Login</div>
+      <div className={`${styles.textContent}`}>Login to your account.</div>
+
+      <form name="login-form" onSubmit={handleSubmit}>
+        <div className={`${styles.formGroup}`}>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" required minLength="8" />
+        </div>
+        <div className={`${styles.formGroup}`}>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required minLength="8" />
+        </div>
+        <button type="submit" className={`${styles.buttonLight}`}>LOGIN</button>
+      </form>
+    </div>
   );
 }
 
--- client/app/src/pages/UserRegister.js
+++ client/app/src/pages/UserRegister.js
@@ -1,6 +1,59 @@
+import React from 'react';
+import styles from "../assets/UserStyle.module.css";
+
 function UserRegister() {
-  return(
-    <div className=''>UserRegister</div>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+      //
+  };
+
+  const handlePasswordConfirmation = (event) => {
+    const password = document.getElementById('password').value;
+    const passwordConfirmation = event.target.value;
+
+    if (password !== passwordConfirmation) {
+      event.target.setCustomValidity('Passwords do not match');
+    } else {
+      event.target.setCustomValidity('');
+    }
+  };
+
+  return (
+    <div className={`${styles.loginPage}`}>
+      <div className={`${styles.gymLogo}`}>
+        <img className={`${styles.topLogo}`} src="https://cdn.dribbble.com/users/6177297/screenshots/20141188/media/766dc94001d4ef551ba405786fbbb323.jpg?resize=400x0" alt="gym-logo"></img>
+      </div>
+      <div className={`${styles.textTitle}`}>TWEAK GYM</div>
+      <div className={`${styles.subTitle}`}>Register</div>
+      <div className={`${styles.textContent}`}>Register your account.</div>
+
+      <form name="login-form" onSubmit={handleSubmit}>
+        <div className={`${styles.formGroup}`}>
+          <label htmlFor="fullname">Full Name:</label>
+          <input type="text" id="fullname" name="fullname" required minLength="8" />
+        </div>
+        <div className={`${styles.formGroup}`}>
+          <label htmlFor="username">Username:</label>
+          <input type="text" id="username" name="username" required minLength="8" />
+        </div>
+        <div className={`${styles.formGroup}`}>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" required minLength="8" />
+        </div>
+        <div className={`${styles.formGroup}`}>
+          <label htmlFor="password-confirmation">Password Confirmation:</label>
+          <input
+            type="password"
+            id="password-confirmation"
+            name="password-confirmation"
+            required
+            minLength="8"
+            onChange={handlePasswordConfirmation}
+          />
+        </div>
+        <button type="submit" className={`${styles.buttonLight}`}>LOGIN</button>
+      </form>
+    </div>
   );
 }
 
