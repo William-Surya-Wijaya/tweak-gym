2b41926c16e7eee97ae9e357d48aa07a86a08846
황진홍 <william1412.surya@gmail.com>
2023-12-05 21:08:47 +0700

    Login Up

--- client/app/src/pages/UserLogin.js
+++ client/app/src/pages/UserLogin.js
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "../assets/UserStyle.module.css";
+import axios from "axios";
+import Swal from "sweetalert2";
 
 function UserLogin() {
-  const handleSubmit = (event) => {
+  const [formData, setFormData] = useState({
+    user_email: "",
+    user_password: "",
+  });
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = async (event) => {
     event.preventDefault();
-      //
+    try {
+      const response = await axios.post(
+        "http://localhost:3000/login-tweak-account",
+        formData,
+        {
+          headers: {
+            "x-api-key":
+              "6924e5a89d788bb511a821e8e6534ac278e964510c6dcaf1d33495b123659191352c0150b2584d9c709b4a13052c0664f07334789572dd0e943a3566dcc1659d",
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        // Lakukan sesuatu setelah permintaan berhasil
+      }
+    } catch (error) {
+      // Tangani kesalahan
+    }
   };
 
   return (
@@ -18,12 +44,28 @@ function UserLogin() {
 
       <form name="login-form" onSubmit={handleSubmit}>
         <div className={`${styles.formGroup}`}>
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" required minLength="8" />
+          <label htmlFor="user_email">Email:</label>
+          <input
+            type="text"
+            id="user_email"
+            name="user_email"
+            required
+            minLength="8"
+            value={formData.user_email}
+            onChange={handleInputChange}
+          />
         </div>
         <div className={`${styles.formGroup}`}>
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required minLength="8" />
+          <label htmlFor="user_password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="user_password"
+            required
+            minLength="8"
+            value={formData.user_password}
+            onChange={handleInputChange}
+          />
         </div>
         <button type="submit" className={`${styles.buttonLight}`}>LOGIN</button>
       </form>
